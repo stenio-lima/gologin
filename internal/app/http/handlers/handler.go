07480c10004f1e731307661da72e233652bcf388
Stênio Lima <steniolima91@outlook.com>
2024-04-18 14:19:17 -0300

    feat: create separete files for route handlers

--- internal/app/http/handlers/handler.go
+++ internal/app/http/handlers/handler.go
@@ -1,15 +1,1 @@
 package handlers
-
-import "github.com/gofiber/fiber/v3"
-
-func CreateLoginHandler(c fiber.Ctx) error {
-	return c.JSON("")
-}
-
-func CreateRegisterHandler(c fiber.Ctx) error {
-	return c.JSON("")
-}
-
-func ForgotPasswordHandler(c fiber.Ctx) error {
-	return c.JSON("")
-}
