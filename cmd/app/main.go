b27df539122ca76660981081d9cc7139b63b8a9b
Stênio Lima <steniolima91@outlook.com>
2024-04-22 13:53:51 -0300

    feat: add custom logger

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"github.com/stenio-lima/gologin/internal/app/database/config"
 	"github.com/stenio-lima/gologin/internal/app/http/router"
 )
 
-func main() {
+var (
+	logger *config.Logger
+)
 
+func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.InitDB()
+	if err != nil {
+		logger.Errorf("config database init failed: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Init()
 }
