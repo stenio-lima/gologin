6bb911009e0d334eb3269a92b0b10920354304ec
Stênio Lima <steniolima91@outlook.com>
2024-04-23 12:10:18 -0300

    refactor: switch name to publicRoutes

--- internal/app/http/router/router.go
+++ internal/app/http/router/router.go
@@ -2,16 +2,12 @@ package router
 
 import (
 	"github.com/gofiber/fiber/v3"
-	"log"
 )
 
-func Init() {
-	// Inicializa o Router com as configurações padrão do Fiber
+func InitRoutes() *fiber.App {
 	router := fiber.New()
 
-	// Inicializar as Rotas
-	initRoutes(router)
+	publicRoutes(router)
 
-	// Inicia o server
-	log.Fatal(router.Listen(":3000"))
+	return router
 }
--- internal/app/http/router/routes.go
+++ internal/app/http/router/routes.go
@@ -5,12 +5,11 @@ import (
 	"github.com/stenio-lima/gologin/internal/app/http/handlers"
 )
 
-func initRoutes(router *fiber.App) {
+func publicRoutes(router *fiber.App) {
 	v1 := router.Group("/api/v1")
 	{
 		v1.Post("/login", handlers.CreateLoginHandler)
 		v1.Post("/register", handlers.CreateRegisterHandler)
 		v1.Post("/forgot-password", handlers.ForgotPasswordHandler)
-
 	}
 }
